df550763e25c1d8d9d55217a4b93d342927f0b17
Sunil Prajapati <nrk_619@yahoo.com>
2016-09-25 19:36:51 +0000

    Date print

---
+++ task_01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A module about printing time"""
+
+import datetime
+
+CURDATE = None
+
+def get_current_date():
+    
+     return datetime.date.today()
